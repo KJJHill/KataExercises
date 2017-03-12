17666268eed94abff49292d425f140469cdaae5b
Kendallene Hill <kendallene@gmail.com>
2017-03-12 10:36:15 -0400

    m3-w2d3 Wednesday, March 8, 2017 Homework Complete.

--- m3-w2d3-session-exercises/TechElevator.Web/Controllers/HomeController.cs
+++ m3-w2d3-session-exercises/TechElevator.Web/Controllers/HomeController.cs
@@ -3,20 +3,56 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using TechElevator.Web.Models;
 
 namespace TechElevator.Web.Controllers
 {
     public class HomeController : Controller
     {
+
         // GET: Home
         public ActionResult Index()
         {
             return View("Index");
         }
 
-        public ActionResult WhyUs()
+        public ActionResult Page1()
         {
-            return View("WhyUs");
+            return View();
+        }
+        public ActionResult Page2()
+        {
+            if (!String.IsNullOrEmpty(Request["color"]))
+            {
+                Session["color"] = Request["color"];
+            }
+
+            return View();
+        }
+
+        public ActionResult Page3()
+        {
+            if (!String.IsNullOrEmpty(Request["food"]))
+            {
+                Session["food"] = Request["food"];
+            }
+
+            return View();
+        }
+
+        public ActionResult Summary()
+        {
+            if (!String.IsNullOrEmpty(Request["season"]))
+            {
+                Session["season"] = Request["season"];
+            }
+
+            FavoriteThings fv = new FavoriteThings();
+            fv.Color = Session["color"].ToString();
+            fv.Food = Session["food"].ToString();
+            fv.Season = Session["season"].ToString();
+
+            return View(fv);
         }
     }
 }
---
+++ m3-w2d3-session-exercises/TechElevator.Web/Models/FavoriteThings.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TechElevator.Web.Models
+{
+    public class FavoriteThings
+    {
+        public string Color { get; set; }
+
+        public string Food { get; set; }
+
+        public string Season { get; set; }
+
+    }
+}
