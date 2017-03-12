c98a332ab361e8e289af51e7f902ad5a11616738
Kendallene Hill <kendallene@gmail.com>
2017-03-12 00:00:21 -0500

    m3-w2d2 Tuesday, March 7, 2017 homework complete

--- m3-w2d2-controllers-part2-exercises/FormsWithHttpPost/Controllers/HomeController.cs
+++ m3-w2d2-controllers-part2-exercises/FormsWithHttpPost/Controllers/HomeController.cs
@@ -10,12 +10,40 @@ namespace FormsWithHttpPost.Controllers
 {
     public class HomeController : Controller
     {
-        
+
 
         // GET: Home
         public ActionResult Index()
         {
             return View();
-        }        
+        }
+
+        public ActionResult NewReview()
+        {
+            return View();
+        }
+
+        public ActionResult SeeAllReviews()
+        {
+            List<Review> results = new List<Review>();
+
+            IReviewDAL dal = new ReviewSqlDAL();
+            
+            if (!String.IsNullOrEmpty(Request["username"]) && !String.IsNullOrEmpty(Request["rating"])
+                && !String.IsNullOrEmpty(Request["title"]) && !String.IsNullOrEmpty(Request["message"]))
+            {
+                Review r = new Review();
+                r.Username = Request["username"];
+                r.Rating = int.Parse(Request["rating"]);
+                r.Title = Request["title"];
+                r.Message = Request["message"];
+
+                dal.SaveReview(r);
+            }
+
+            results = dal.GetAllReviews();
+
+            return View(results);
+        }
     }
 }
--- m3-w2d2-controllers-part2-exercises/FormsWithHttpPost/DAL/ReviewSqlDAL.cs
+++ m3-w2d2-controllers-part2-exercises/FormsWithHttpPost/DAL/ReviewSqlDAL.cs
@@ -9,16 +9,71 @@ namespace FormsWithHttpPost.DAL
 {
     public class ReviewSqlDAL : IReviewDAL
     {
-
+        string connectionString = @"Data Source=DESKTOP-ICT08NO\SQLEXPRESS;Initial Catalog=BookReview;Integrated Security=True";
+        string SQL_GetAllReviews = @"Select username, rating, title, message, review_date, id from review;";
+        string SQL_SaveReview = @"Insert into review (username, rating, title, message, review_date) values (@username, @rating, @title, @message, GETDATE())";
 
         public List<Review> GetAllReviews()
         {
-            throw new NotImplementedException();
+            List<Review> result = new List<Review>();
+
+            try
+            {
+                using (SqlConnection conn = new SqlConnection(connectionString))
+                {
+                    conn.Open();
+
+                    SqlCommand cmd = new SqlCommand(SQL_GetAllReviews, conn);
+
+                    SqlDataReader reader = cmd.ExecuteReader();
+
+                    while (reader.Read())
+                    {
+                        Review r = new Review();
+                        r.Id = Convert.ToInt32(reader["id"]);
+                        r.Username = Convert.ToString(reader["username"]);
+                        r.Rating = Convert.ToInt32(reader["rating"]);
+                        r.Title = Convert.ToString(reader["title"]);
+                        r.Message = Convert.ToString(reader["message"]);
+                        r.ReviewDate = Convert.ToDateTime(reader["review_date"]);
+
+                        result.Add(r);
+                    }
+
+                }
+            }
+            catch (SqlException ex)
+            {
+                throw new NotImplementedException();
+            }
+
+            return result;
         }
 
         public bool SaveReview(Review newReview)
         {
-            throw new NotImplementedException();
+            try
+            {
+                using (SqlConnection conn = new SqlConnection(connectionString))
+                {
+                    conn.Open();
+
+                    SqlCommand cmd = new SqlCommand(SQL_SaveReview, conn);
+
+                    cmd.Parameters.AddWithValue("@username", newReview.Username);
+                    cmd.Parameters.AddWithValue("@rating", newReview.Rating);
+                    cmd.Parameters.AddWithValue("@title", newReview.Title);
+                    cmd.Parameters.AddWithValue("@message", newReview.Message);
+
+                    int rowsAffected = cmd.ExecuteNonQuery();
+
+                    return (rowsAffected == 1);
+                }
+            }
+            catch (SqlException ex)
+            {
+                throw new NotImplementedException();
+            }
         }
     }
 }
